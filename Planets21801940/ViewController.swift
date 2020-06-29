e955c05c4ef7574a80d801680b56db06b26e2945
Thumendes <arthurmendespereira505@gmail.com>
2020-06-29 08:38:47 -0300

    Adicionar arquivos do XCode

---
+++ Planets21801940/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Planets21801940
+//
+//  Created by COTEMIG on 22/06/20.
+//  Copyright © 2020 Cotemig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "Linha"
+    }
+    
+
+    @IBOutlet weak var Table: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        Table.dataSource = self
+        Table.delegate = self
+    }
+}
+
